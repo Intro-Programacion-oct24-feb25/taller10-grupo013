b59a2da8eedba1d2d1e57ce69f0c795505563557
unknown <luiseduardosa2006@gmail.com>
2024-12-19 11:51:08 -0500

    Taller 10

--- Taller10/src/paquete1/Problema01.java
+++ Taller10/src/paquete1/Problema01.java
@@ -15,6 +15,28 @@ public class Problema01 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
+        double[][] notas = {{9, 10, 7, 8},
+        {10, 5, 7, 9},
+        {7, 9.4, 10, 9}};
+        String[] nombresEstudiante = {"Jerry Ponce", "Gabriela Lewis", "David Bell"};
+        double[] promedios = new double[3];
+        double suma;
+
+        for (int i = 0; i < notas.length; i++) {
+            suma = 0; // 0
+            for (int j = 0; j < notas[i].length; j++) {
+                suma = suma + notas[i][j];
+            }
+            promedios[i] = suma / notas[i].length;
+
+           
+
+                
+            }
+         for (int i = 0; i < notas.length; i++) {
+               
+                    System.out.printf("Estudiante: %s tiene un promedio de %.2f\n", nombresEstudiante[i], promedios[i]);
+        }
     
+    }
 }
