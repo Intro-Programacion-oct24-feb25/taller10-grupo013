cb8116f4e3c27423b7981c0f9d5bdda54264a60b
unknown <luiseduardosa2006@gmail.com>
2024-12-19 12:21:00 -0500

    hola

--- Taller10/src/paquete2/Problema02.java
+++ Taller10/src/paquete2/Problema02.java
@@ -5,10 +5,34 @@
  */
 package paquete2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class Problema02 {
-    
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        String[] nombres = {"Jessica Cole", "Robert Wallace"};
+        double[][] ventas = new double[2][5];
+        double suma = 0;
+        double ingresoVentas;
+
+        for (int i = 0; i < ventas.length; i++) {
+            for (int j = 0; j < ventas[i].length; j++) {
+                System.out.println("Ingrese las ventas del dia de " + nombres[i]);
+                ingresoVentas = entrada.nextDouble();
+                ventas[i][j] = ingresoVentas;
+                suma = suma + ingresoVentas;
+            }
+        }
+        for (int i = 0; i < nombres.length; i++) {
+            System.out.printf("Vendedor(a) %s\n", nombres[i]);
+
+        }
+        System.out.printf("han realizado un total de %.2f en ventas", suma);
+
+    }
 }
