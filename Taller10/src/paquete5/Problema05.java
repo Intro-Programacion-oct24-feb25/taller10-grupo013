77b2408a1352c2a19316ca549f2bf55936493ab0
unknown <luiseduardosa2006@gmail.com>
2024-12-19 12:54:16 -0500

    hola

--- Taller10/src/paquete5/Problema05.java
+++ Taller10/src/paquete5/Problema05.java
@@ -10,5 +10,25 @@ package paquete5;
  * @author reroes
  */
 public class Problema05 {
-    
+
+    public static void main(String[] args) {
+        String[][] estudiantes = {{"Sarah Ward", "Philip Payne"},
+        {"Carrie Burton", "Lauren Rice"},
+        {"Emma Escobar", "Lori Flores"},
+        {"Steven West", "Toni Martin"}
+        };
+        String inicial;
+        String cadena = "";
+
+        for (int i = 0; i < estudiantes.length; i++) {
+            for (int j = 0; j < estudiantes[i].length; j++) {
+                inicial = estudiantes[i][j].substring(0,1);
+                if (inicial.equals("S") || inicial.equals("P") || inicial.equals("T")) {
+                    cadena = String.format("%s%s\n", cadena, estudiantes[i][j]);
+                }
+            }
+        }
+        System.out.printf("Estudiantes del arreglo con inicial: S,P,T:\n%s",
+                cadena);
+    }
 }
